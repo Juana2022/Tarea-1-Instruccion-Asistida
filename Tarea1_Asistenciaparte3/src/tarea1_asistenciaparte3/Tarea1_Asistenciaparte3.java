87b9fd252c16cbbfffaa76881546a9ede0ca65e0
Juana2022 <149335279+Juana2022@users.noreply.github.com>
2023-10-29 22:47:24 -0500

    Add files via upload

---
+++ Tarea1_Asistenciaparte3/src/tarea1_asistenciaparte3/Tarea1_Asistenciaparte3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1_asistenciaparte3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jalcivar4379@utm.edu.ec
+ */
+public class Tarea1_Asistenciaparte3 {
+
+     Scanner entrada = new Scanner(System.in);
+
+private int x = 6; 
+private int y = 6; 
+private Random Aleatorios = new Random();
+private int numero1; 
+private int numero2;
+private int intentos = 0;
+private int aciertos = 0;
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
