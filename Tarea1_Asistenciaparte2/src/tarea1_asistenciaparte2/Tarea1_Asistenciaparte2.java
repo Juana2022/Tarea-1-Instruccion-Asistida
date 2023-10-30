ae2b41ac1d9090b819b17c73719830cfed4fd984
Juana2022 <149335279+Juana2022@users.noreply.github.com>
2023-10-29 22:46:58 -0500

    Add files via upload

---
+++ Tarea1_Asistenciaparte2/src/tarea1_asistenciaparte2/Tarea1_Asistenciaparte2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1_asistenciaparte2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jalcivar4379@utm.edu.ec
+ */
+public class Tarea1_Asistenciaparte2 {
+    
+     Scanner entrada = new Scanner(System.in);
+
+private int x = 6; 
+private int y = 6; 
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
